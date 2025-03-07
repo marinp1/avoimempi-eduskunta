075cda3577cea6e015e22f224f68ae36ff062613
Patrik Marin <patrik.marin@nitor.com>
2025-03-07 14:35:28 +0200

    fix(db): improve db functions

--- migrations/V001.003__create_current_lookup.sql
+++ migrations/V001.003__create_current_lookup.sql
@@ -3,12 +3,12 @@ CREATE INDEX idx_term_start_date ON term(start_date);
 -- Create index on term.end_date
 CREATE INDEX idx_term_end_date ON term(end_date);
 -- Create index on temporaryabsences.start_date
-CREATE INDEX idx_temporaryabsences_start_date ON temporaryabsences(start_date);
+CREATE INDEX idx_temporaryabsence_start_date ON temporaryabsence(start_date);
 -- Create index on temporaryabsences.end_date
-CREATE INDEX idx_temporaryabsences_end_date ON temporaryabsences(end_date);
+CREATE INDEX idx_temporaryabsence_end_date ON temporaryabsence(end_date);
 
 REINDEX TABLE term;
-REINDEX TABLE temporaryabsences;
+REINDEX TABLE temporaryabsence;
 
 -- Create a database function to get the parliament composition
 CREATE OR REPLACE FUNCTION getParliamentComposition(datestr DATE)
@@ -28,44 +28,32 @@ RETURNS TABLE (
 ) AS $$
 BEGIN
     RETURN QUERY
-WITH ActiveRepresentatives AS (
-    SELECT
-        r.person_id,
-        r.last_name,
-        r.first_name,
-        r.sort_name,
-        r.gender,
-        r.birth_date,
-        r.birth_place,
-        r.death_date,
-        r.death_place,
-        r.profession,
-        t.start_date AS t_start_date,
-        t.end_date AS t_end_date
-    FROM
-        representative r
-    JOIN
-        term t ON r.person_id = t.person_id
-    WHERE
-            t.start_date <= datestr
-            AND (t.end_date IS NULL OR t.end_date >= datestr)
-),
-ActiveInterruptions AS (
-    SELECT
-        ta.person_id
-    FROM
-        temporaryabsences ta
-    WHERE
-        ta.start_date <= datestr
-        AND (ta.end_date IS NULL OR ta.end_date >= datestr)
-)
 SELECT
-    ar.*
+    r.person_id,
+    r.last_name,
+    r.first_name,
+    r.sort_name,
+    r.gender,
+    r.birth_date,
+    r.birth_place,
+    r.death_date,
+    r.death_place,
+    r.profession,
+    t.start_date AS t_start_date,
+    t.end_date AS t_end_date
 FROM
-    ActiveRepresentatives ar
-LEFT JOIN
-    ActiveInterruptions ai ON ar.person_id = ai.person_id
+    representative r
+JOIN
+    term t ON r.person_id = t.person_id
 WHERE
-    ai.person_id IS NULL;
+    t.start_date <= datestr
+    AND (t.end_date IS NULL OR t.end_date >= datestr)
+    AND NOT EXISTS (
+        SELECT 1
+        FROM temporaryabsence ta
+        WHERE ta.person_id = r.person_id
+          AND ta.start_date <= datestr
+          AND (ta.end_date IS NULL OR ta.end_date >= datestr)
+    );
 END;
 $$ LANGUAGE plpgsql;
