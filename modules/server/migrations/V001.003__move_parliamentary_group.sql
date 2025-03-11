7f8fc4aec56be937215e4a356180133f4fdb8dd9
Patrik Marin <patrik.marin@nitor.com>
2025-03-11 13:47:55 +0200

    refactor(db): squash migration files

--- modules/server/migrations/V001.003__move_parliamentary_group.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ParliamentaryGroupMembership
-ADD COLUMN group_name VARCHAR(255);
-
-ALTER TABLE ParliamentaryGroupAssignment
-ADD COLUMN group_name VARCHAR(255);
-
-ALTER TABLE ParliamentaryGroup
-DROP COLUMN name;
