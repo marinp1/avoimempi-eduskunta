b09d78f488be44754c108b66066fd8668cfc7b65
Patrik Marin <patrik.marin@nitor.com>
2025-03-11 20:39:25 +0200

    style(app): improve seating styles

--- application/client/Representatives/RepresentativeAvatar.tsx
+++ application/client/Representatives/RepresentativeAvatar.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 export const RepresentativeAvatar: React.FC<
-  DatabaseFunctions.GetParliamentComposition
+  DatabaseFunctions.GetParliamentComposition & {
+    transform: {
+      y: number;
+    };
+  }
 > = (person) => {
   const initials =
     `${person.first_name[0]}${person.last_name[0]}`.toUpperCase();
   const fullName = `${person.first_name} ${person.last_name}`;
+
   return (
-    <div className="representative-circle" title={fullName}>
+    <div
+      className="representative-circle"
+      title={fullName}
+      style={{
+        transform: `translate(0%, ${person.transform.y}%)`,
+      }}
+    >
       {initials}
     </div>
   );
