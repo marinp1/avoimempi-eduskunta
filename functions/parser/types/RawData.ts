eaa1eed39588ffe6302ecb6e1a19f69a102a0e87
Patrik Marin <patrik.marin@nitor.com>
2025-03-13 17:07:54 +0200

    feat(parser): add types for voting content

--- functions/parser/types/RawData.ts
+++ functions/parser/types/RawData.ts
@@ -1,8 +1,12 @@
 import { TableName } from "#constants/TableNames";
 
+type StringifiedNumber = string;
+type StringifiedDate = string;
+type StringifiedBoolean = "1" | "0";
+
 interface RawDataModels {
   [TableName.MemberOfParliament]: {
-    personId: number;
+    personId: StringifiedNumber;
     XmlData: string;
     XmlDataEn: string;
     XmlDataFi: string;
@@ -11,6 +15,53 @@ interface RawDataModels {
     lastname: string;
     minister: string;
     party: string;
+  };
+  [TableName.SaliDBAanestys]: {
+    AanestysId: StringifiedNumber;
+    AanestysAlkuaika: string;
+    AanestysLisaOtsikko: string;
+    AanestysLoppuaika: string;
+    AanestysMitatoity: StringifiedBoolean;
+    AanestysNumero: StringifiedNumber;
+    AanestysOtsikko: string;
+    AanestysPoytakirja: string;
+    AanestysPoytakirjaUrl: string;
+    AanestysTulosEi: StringifiedNumber;
+    AanestysTulosJaa: StringifiedNumber;
+    AanestysTulosPoissa: StringifiedNumber;
+    AanestysTulosTyhjia: StringifiedNumber;
+    AanestysTulosYhteensa: StringifiedNumber;
+    AanestysValtiopaivaasia: string;
+    AanestysValtiopaivaasiaUrl: string;
+    AliKohtaTunniste: string;
+    Imported: StringifiedDate;
+    IstuntoAlkuaika: StringifiedDate;
+    IstuntoIlmoitettuAlkuaika: StringifiedDate;
+    IstuntoNumero: StringifiedNumber;
+    IstuntoPvm: StringifiedDate;
+    IstuntoVPVuosi: StringifiedNumber;
+    KieliId: Modules.Common.LanguageId;
+    KohtaHuomautus: string;
+    KohtaJarjestys: StringifiedNumber;
+    KohtaKasittelyOtsikko: string;
+    KohtaKasittelyVaihe: string;
+    KohtaOtsikko: string;
+    KohtaTunniste: StringifiedNumber;
+    PJOtsikko: string;
+    PaaKohtaHuomautus: string;
+    PaaKohtaOtsikko: string;
+    PaaKohtaTunniste: string;
+    Url: string;
+  };
+  [TableName.SaliDBAanestysEdustaja]: {
+    EdustajaId: StringifiedNumber;
+    AanestysId: StringifiedNumber;
+    EdustajaAanestys: Modules.Common.VoteResult;
+    EdustajaEtunimi: string;
+    EdustajaHenkiloNumero: StringifiedNumber;
+    EdustajaRyhmaLyhenne: string;
+    EdustajaSukunimi: string;
+    Imported: StringifiedDate;
   };
 }
 
--- shared/typings/index.ts
+++ shared/typings/index.ts
@@ -1,7 +1,13 @@
-import type { TableNames } from "../constants/TableNames.ts";
+import type {
+  TableNames,
+  VoteResults,
+  LanguageIds,
+} from "../constants/TableNames.ts";
 
 declare global {
   namespace Modules.Common {
     export type TableName = (typeof TableNames)[number];
+    export type VoteResult = (typeof VoteResults)[keyof typeof VoteResults];
+    export type LanguageId = (typeof LanguageIds)[keyof typeof LanguageIds];
   }
 }
