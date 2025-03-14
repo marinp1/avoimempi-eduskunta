cb5ebfb5e86770a7a418e42e49a4c8ab40823d6a
Patrik Marin <patrik.marin@nitor.com>
2025-03-14 15:38:02 +0200

    feat(parser): save data to sqlite database

--- shared/database/index.ts
+++ shared/database/index.ts
@@ -3,3 +3,11 @@ import path from "path";
 export const getDatabasePath = () => {
   return path.join(import.meta.dirname, "../../avoimempi-eduskunta.db");
 };
+
+export const getRawDatabasePath = () => {
+  return path.join(import.meta.dirname, "../../eduskunta-raw-data.db");
+};
+
+export const getParsedDatabasePath = () => {
+  return path.join(import.meta.dirname, "../../eduskunta-parsed-data.db");
+};
