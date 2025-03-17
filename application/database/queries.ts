d3c5bd52097f1b5879b1208fc4d21e70b13f56d0
Patrik Marin <patrik.marin@nitor.com>
2025-03-17 13:16:50 +0200

    fix(app): fix query after sqlite

--- application/database/queries.ts
+++ application/database/queries.ts
@@ -20,12 +20,12 @@ JOIN
     term t ON r.person_id = t.person_id
 WHERE
     t.start_date <= $date
-    AND (t.end_date IS NULL OR t.end_date >= $date)
+    AND (t.end_date = '' OR t.end_date >= $date)
     AND NOT EXISTS (
         SELECT 1
         FROM temporaryabsence ta
         WHERE ta.person_id = r.person_id
           AND ta.start_date <= $date
-          AND (ta.end_date IS NULL OR ta.end_date >= $date)
+          AND (ta.end_date = '' OR ta.end_date >= $date)
     )
     `;
