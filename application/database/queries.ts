55c5cc1a12564575ee932f633b4816999c4af00e
Patrik Marin <patrik.marin@nitor.com>
2025-11-13 12:04:03 +0200

    feat(app): add composition to parliament

--- application/database/queries.ts
+++ application/database/queries.ts
@@ -13,11 +13,29 @@ SELECT
     r.death_place,
     r.profession,
     t.start_date AS t_start_date,
-    t.end_date AS t_end_date
+    t.end_date AS t_end_date,
+    pgm.group_name AS party_name,
+    CASE
+        WHEN pgm.group_name IN (
+            SELECT DISTINCT pgm2.group_name
+            FROM GovernmentMembership gm
+            JOIN Representative r2 ON gm.person_id = r2.person_id
+            JOIN ParliamentaryGroupMembership pgm2 ON r2.person_id = pgm2.person_id
+            WHERE gm.start_date <= $date
+              AND (gm.end_date = '' OR gm.end_date >= $date)
+              AND pgm2.start_date <= $date
+              AND (pgm2.end_date = '' OR pgm2.end_date >= $date)
+        ) THEN 1
+        ELSE 0
+    END AS is_in_government
 FROM
     representative r
 JOIN
     term t ON r.person_id = t.person_id
+LEFT JOIN
+    ParliamentaryGroupMembership pgm ON r.person_id = pgm.person_id
+    AND pgm.start_date <= $date
+    AND (pgm.end_date = '' OR pgm.end_date >= $date)
 WHERE
     t.start_date <= $date
     AND (t.end_date = '' OR t.end_date >= $date)
