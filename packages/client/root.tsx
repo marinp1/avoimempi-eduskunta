026be580d23b96b0092f903f1e8526a3299085f3
Patrik Marin <dev@patrikmarin.fi>
2026-03-05 05:50:35 +0000

    fix(client): shim d3 library
    Bun tree-shakes code that is really needed.

--- packages/client/root.tsx
+++ packages/client/root.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import { HallituskausiProvider } from "./filters/HallituskausiContext";
 import "./i18n";
+import "./utils/d3-scale-shim";
 import { ThemeProvider } from "./theme/ThemeContext";
 
 document.addEventListener("DOMContentLoaded", () => {
